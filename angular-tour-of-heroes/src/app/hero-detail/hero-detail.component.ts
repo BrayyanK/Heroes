8fb201766060659b07394b2fa3671cca5c73432d
Brayyan <bkostadinov@somos.plus>
2022-03-21 12:50:22 +0100

    Heroe

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { hero } from '../hero';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detalles.component.html',
-  styleUrls: ['./hero-detalles.component.scss']
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: hero;
+  @Input() hero?: Hero;
 
   constructor() { }
 
